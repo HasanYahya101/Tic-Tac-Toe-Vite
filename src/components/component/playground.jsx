e09d609136bd99da31635bfe4fca06a69212f83e
Hasan Yahya <hasna786yahya@gmail.com>
2024-05-28 05:13:20 +0500

    refactor: Remove unnecessary whitespace in Playground component

--- src/components/component/playground.jsx
+++ src/components/component/playground.jsx
@@ -160,6 +160,7 @@ export function Playground() {
     }
 
     return (
+
         <div className="flex flex-col items-center justify-center h-screen gap-8">
             <Toaster />
             <div className="flex content-center">
